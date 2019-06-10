d45cecd46d41f5b0b895e5a5d04e744012c14294
mapuaptu <mapuaptu@gmail.com>
2019-06-10 17:17:46 +0400

    Сгенерировал проект через vue-cli.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import Button from "./packages/button";
+import Button from './packages/button';
 
 const components = { Button };
 
-const install = Vue => {
+const install = (Vue) => {
   for (const key in components) {
     const component = components[key];
 
@@ -10,11 +10,11 @@ const install = Vue => {
   }
 };
 
-if (typeof window !== "undefined" && window.Vue) {
+if (typeof window !== 'undefined' && window.Vue) {
   install(window.Vue);
 }
 
 export default {
   install,
-  ...components
+  ...components,
 };
