f4de585b559876fe9f309172222d3304bc23179d
mapuaptu <mapuaptu@gmail.com>
2019-06-11 16:34:24 +0400

    Переписал цикл for in что-бы исправить ошибку eslint.

--- src/index.js
+++ src/index.js
@@ -4,15 +4,8 @@ const components = {
   Button,
 };
 
-// TODO: поправить eslint
-
-const install = (Vue) => {
-  for (let key in components) {
-    let component = components[key];
-    Vue.component(component.name, component);
-  }
+export default (Vue) => {
+  Object.values(components).map((component) => Vue.component(component.name, component));
 };
-
-export default install;
 
 export { Button };
