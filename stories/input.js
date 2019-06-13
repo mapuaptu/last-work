0fa59abf4639d39d3d2f4de19406390b643aaa22
mapuaptu <mapuaptu@gmail.com>
2019-06-13 21:50:08 +0400

    Добавил состояние focus и empty field.

--- stories/input.js
+++ stories/input.js
@@ -2,9 +2,17 @@ import { storiesOf } from '@storybook/vue';
 import { Input } from '../src/index';
 
 export default () =>
-  storiesOf('Input', module).add('default', () => ({
-    components: {
-      'ui-input': Input,
-    },
-    template: '<ui-input value="Example text field"></ui-input>',
-  }));
+  storiesOf('Input', module)
+    .add('default', () => ({
+      components: {
+        'ui-input': Input,
+      },
+      template:
+        '<ui-input placeholder="Example placeholder" value="Example text field"></ui-input>',
+    }))
+    .add('empty', () => ({
+      components: {
+        'ui-input': Input,
+      },
+      template: '<ui-input placeholder="Example placeholder"></ui-input>',
+    }));
