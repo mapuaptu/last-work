a01292a0dc8bc03dc4da5530ee79e7f27f13caa6
mapuaptu <mapuaptu@gmail.com>
2019-06-13 22:38:20 +0400

    Рефакторинг компонента checkbox.

--- src/packages/checkbox/src/checkbox.js
+++ src/packages/checkbox/src/checkbox.js
@@ -13,32 +13,5 @@ export default {
       type: String,
       default: '',
     },
-    name: {
-      type: String,
-      default: '',
-    },
-    size: {
-      type: String,
-      default: '',
-    },
-  },
-  data() {
-    return {
-      focus: false,
-    };
-  },
-  computed: {
-    classes() {
-      return {
-        'is-disabled': this.disabled,
-        'is-checked': this.value,
-        [`is-${this.size}`]: this.size,
-      };
-    },
-  },
-  methods: {
-    handleInput(event) {
-      this.$emit('input', event.target.checked);
-    },
   },
 };
---
+++ src/packages/checkbox/src/checkbox.vue
@@ -0,0 +1,24 @@
+<template>
+  <label :class="[$style.wrapper]">
+    <div>
+      <span :class="[$style.inner]"></span>
+      <input
+        :class="[$style.input]"
+        type="checkbox"
+        :checked="value"
+        :disabled="disabled"
+        v-bind="$attrs"
+        v-on="$listeners"
+      >
+    </div>
+    <div
+      :class="[$style.label]"
+      v-if="$slots.default"
+    >
+      <slot></slot>
+    </div>
+  </label>
+</template>
+
+<script src="./checkbox.js"></script>
+<style src="./checkbox.scss" lang="scss" module></style>
