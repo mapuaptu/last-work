d5d078e42294c18bdcad53227ca04d8b9b88e1e9
mapuaptu <mapuaptu@gmail.com>
2019-06-18 17:37:31 +0400

    Захардкодил svg в logo.

--- src/packages/logo/src/logo.js
+++ src/packages/logo/src/logo.js
@@ -1,3 +1,13 @@
 export default {
   name: 'dd-logo',
+  props: {
+    width: {
+      type: Number,
+      default: 380,
+    },
+    height: {
+      type: Number,
+      default: 50,
+    },
+  },
 };
