e209d0bc77d2399c0c060a941147c44e7a7ff3e9
mapuaptu <mapuaptu@gmail.com>
2019-06-21 17:34:55 +0400

    Убрал лишние stories для button.

--- stories/button.js
+++ stories/button.js
@@ -2,7 +2,7 @@ import { storiesOf } from '@storybook/vue';
 import { action } from '@storybook/addon-actions';
 import { withInfo } from 'storybook-addon-vue-info';
 import { withKnobs, text, select } from '@storybook/addon-knobs';
-import { Button, Container, Row, Col } from '../src/index';
+import { Button } from '../src/index';
 
 const options = {
   type: {
@@ -54,56 +54,4 @@ export default () =>
           summary: 'Default button',
         },
       },
-    )
-    .add(
-      'size="small"',
-      () => ({
-        components: { 'dd-button': Button },
-        template: '<dd-button size="small">ADD NEW TASK</dd-button>',
-      }),
-      {
-        info: {
-          summary: 'Summary for MyComponent',
-        },
-      },
-    )
-    .add('size="large"', () => ({
-      components: { 'dd-button': Button },
-      template: '<dd-button size="large">ADD NEW TASK</dd-button>',
-    }))
-    .add('type="primary"', () => ({
-      components: { 'dd-button': Button },
-      template: '<dd-button type="primary">ADD NEW TASK</dd-button>',
-    }))
-    .add('type="secondary"', () => ({
-      components: { 'dd-button': Button },
-      template: '<dd-button type="secondary">ADD NEW TASK</dd-button>',
-    }))
-    .add('icon="circle"', () => ({
-      components: { 'dd-button': Button },
-      template: '<dd-button icon="circle">ADD NEW TASK</dd-button>',
-    }))
-    .add(':fluid="false"', () => ({
-      components: {
-        'dd-button': Button,
-        'dd-container': Container,
-        'dd-row': Row,
-        'dd-col': Col,
-      },
-      template: '<dd-button icon="circle">ADD NEW TASK</dd-button>',
-    }))
-    .add(':fluid="true"', () => ({
-      components: {
-        'dd-button': Button,
-        'dd-container': Container,
-        'dd-row': Row,
-        'dd-col': Col,
-      },
-      template: `<dd-container>
-        <dd-row>
-          <dd-col size="12">
-          <dd-button fluid icon="circle">ADD NEW TASK</dd-button>
-          </dd-col>
-        </dd-row>
-      </dd-container>`,
-    }));
+    );
