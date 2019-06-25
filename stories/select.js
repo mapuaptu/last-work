0f2e9da9cea7d376c4bb9ce251cb333ab2630e02
mapuaptu <mapuaptu@gmail.com>
2019-06-25 16:08:41 +0400

    Создал заготоку story под компонент select.

---
+++ stories/select.js
@@ -0,0 +1,24 @@
+import { storiesOf } from '@storybook/vue';
+import { action } from '@storybook/addon-actions';
+import { withInfo } from 'storybook-addon-vue-info';
+import { withKnobs, text, select, boolean } from '@storybook/addon-knobs';
+import { Select } from '../src/index';
+
+const options = {};
+
+export default () =>
+  storiesOf('Select', module)
+    .addDecorator(withInfo)
+    .addDecorator(withKnobs)
+    .add(
+      'Default',
+      () => ({
+        components: { 'dd-select': Select },
+        template: '<dd-select></dd-select>',
+      }),
+      {
+        info: {
+          summary: 'Default select',
+        },
+      },
+    );
