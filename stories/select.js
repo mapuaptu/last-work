8b02bf586d81f57f206c711539a9436c7f35ca92
mapuaptu <mapuaptu@gmail.com>
2019-07-01 21:16:08 +0400

    Рефакторинг компонента select.

--- stories/select.js
+++ stories/select.js
@@ -1,6 +1,7 @@
 import { storiesOf } from '@storybook/vue';
 import { withInfo } from 'storybook-addon-vue-info';
 import { withKnobs, text, boolean } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
 import { Select } from '../src/index';
 
 const options = {
@@ -36,11 +37,13 @@ export default () =>
             default: boolean('Disabled', options.disabled),
           },
         },
-        template: `<div :style="{width: '280px'}">
-          <dd-select v-model="value" :options="countries" :disabled="disabled"
-          :placeholder="placeholder">
-          </dd-select>
-        </div>`,
+        template: `<dd-select v-model="value" :options="countries"
+        :disabled="disabled" :placeholder="placeholder" style="width: 500px"
+        @input="onInput">
+        </dd-select>`,
+        methods: {
+          onInput: action('input'),
+        },
       }),
       {
         info: {
