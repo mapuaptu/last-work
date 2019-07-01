d7db8af22a90d5e575c9a8d2afc417891edb1650
mapuaptu <mapuaptu@gmail.com>
2019-07-01 20:22:22 +0400

    Рефакторинг story userpic.

--- stories/userpic.js
+++ stories/userpic.js
@@ -1,7 +1,14 @@
 import { storiesOf } from '@storybook/vue';
 import { withInfo } from 'storybook-addon-vue-info';
-import { withKnobs } from '@storybook/addon-knobs';
+import { withKnobs, text, number } from '@storybook/addon-knobs';
 import { Userpic } from '../src/index';
+
+const options = {
+  url: `https://gravatar.com/avatar/
+              e40f37c3d4d4cff5a87dd7d476af3fb4?s=200&d=retro&r=x`,
+  alt: 'Alt text',
+  size: 33,
+};
 
 export default () =>
   storiesOf('Userpic', module)
@@ -10,13 +17,16 @@ export default () =>
     .add(
       'Default',
       () => ({
-        data() {
-          return {
-            url: `https://gravatar.com/avatar/
-              e40f37c3d4d4cff5a87dd7d476af3fb4?s=200&d=retro&r=x`,
-            alt: 'Alt text',
-            size: 33,
-          };
+        props: {
+          url: {
+            default: text('Url', options.url),
+          },
+          alt: {
+            default: text('Alt', options.alt),
+          },
+          size: {
+            default: number('Size', options.size),
+          },
         },
         components: { 'dd-userpic': Userpic },
         template: `<dd-userpic :url="url" :alt="alt" :size="size">
