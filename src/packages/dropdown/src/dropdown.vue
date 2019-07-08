39ba317825bcc78c6b2471a0163f6921cba13ea5
mapuaptu <mapuaptu@gmail.com>
2019-07-08 18:29:06 +0400

    Реализовал слот control, для компонента dropdown.

--- src/packages/dropdown/src/dropdown.vue
+++ src/packages/dropdown/src/dropdown.vue
@@ -3,10 +3,21 @@
     :class="$style.dropdown"
     v-click-outside="onClickOutside"
   >
+
+    <div
+      v-if="$slots.control"
+      :class="[$style.control]"
+      @click="onClick"
+    >
+      <slot name="control"></slot>
+    </div>
+
     <i
+      v-else
       :class="[$style.icon, `fa fa-${icon}`]"
       @click="onClick"
     ></i>
+
     <transition name="fade">
       <div
         v-if="value"
