e1d435a709749faa7a5f082f23c911ed65c57510
mapuaptu <mapuaptu@gmail.com>
2019-07-20 14:53:53 +0400

    Добавил тесты для компонента block.

--- src/packages/block/src/block.test.js
+++ src/packages/block/src/block.test.js
@@ -2,9 +2,21 @@ import { mount } from '@vue/test-utils';
 import Block from './block.vue';
 
 describe('Block', () => {
-  test('Block is a Vue instance', () => {
+  test('Block - exists', () => {
     const wrapper = mount(Block);
 
-    expect(wrapper.isVueInstance()).toBeTruthy();
+    expect(wrapper.exists()).toBeTruthy();
+  });
+
+  test('Block - can render default slot', () => {
+    const slotContent = 'Slot content';
+
+    const wrapper = mount(Block, {
+      slots: {
+        default: slotContent,
+      },
+    });
+
+    expect(wrapper.text()).toBe(slotContent);
   });
 });
