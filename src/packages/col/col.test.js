12c72cb7efcaaa9d4c73084b9174d273086aac84
mapuaptu <mapuaptu@gmail.com>
2019-08-01 19:10:22 +0400

    Правка компонента button and col.

--- src/packages/col/col.test.js
+++ src/packages/col/col.test.js
@@ -26,4 +26,6 @@ describe('Col', () => {
 
     expect(wrapper.text()).to.include(message);
   });
+
+  it('4. Col - has paddings when parent have gutter prop.', () => {});
 });
--- src/packages/col/src/col.js
+++ src/packages/col/src/col.js
@@ -1,47 +1,51 @@
 export default {
   name: 'DdCol',
   props: {
+    justify: {
+      type: String,
+      default: null,
+    },
+    align: {
+      type: String,
+      default: null,
+    },
+    self: {
+      type: String,
+      default: null,
+    },
     size: {
       type: [String, Number],
       default: null,
     },
-    xs: {
-      type: String,
-      default: null,
-    },
-    vs: {
-      type: String,
-      default: null,
-    },
-    sm: {
-      type: String,
-      default: null,
-    },
-    md: {
-      type: String,
+    xl: {
+      type: [String, Number],
       default: null,
     },
     lg: {
-      type: String,
+      type: [String, Number],
       default: null,
     },
-    xl: {
-      type: String,
+    md: {
+      type: [String, Number],
+      default: null,
+    },
+    sm: {
+      type: [String, Number],
+      default: null,
+    },
+    vs: {
+      type: [String, Number],
       default: null,
     },
     offsetXs: {
       type: [String, Number],
       default: null,
     },
-    offsetVs: {
+    offset: {
       type: [String, Number],
       default: null,
     },
-    offsetSm: {
-      type: [String, Number],
-      default: null,
-    },
-    offsetMd: {
+    offsetXl: {
       type: [String, Number],
       default: null,
     },
@@ -49,7 +53,15 @@ export default {
       type: [String, Number],
       default: null,
     },
-    offset: {
+    offsetMd: {
+      type: [String, Number],
+      default: null,
+    },
+    offsetSm: {
+      type: [String, Number],
+      default: null,
+    },
+    offsetVs: {
       type: [String, Number],
       default: null,
     },
@@ -77,51 +89,32 @@ export default {
       type: [String, Number],
       default: null,
     },
-    orderXs: {
-      type: [String, Number],
-      default: null,
-    },
-    justify: {
-      type: String,
-      default: null,
-    },
-    align: {
-      type: String,
-      default: null,
-    },
-    self: {
-      type: String,
-      default: null,
-    },
   },
   computed: {
     classList() {
       return [
+        this.$style.flex,
+        this.justify && this.$style[`justify-content-${this.justify}`],
+        this.align && this.$style[`align-items-${this.align}`],
+        this.self && this.$style[`align-self-${this.self}`],
         this.size ? this.$style[`col-${this.size}`] : this.$style['col-12'],
-        this.xs && this.$style[`col-xs-${this.xs}`],
-        this.vs && this.$style[`col-vs-${this.vs}`],
-        this.sm && this.$style[`col-sm-${this.sm}`],
-        this.md && this.$style[`col-md-${this.md}`],
-        this.lg && this.$style[`col-lg-${this.lg}`],
         this.xl && this.$style[`col-xl-${this.xl}`],
+        this.lg && this.$style[`col-lg-${this.lg}`],
+        this.md && this.$style[`col-md-${this.md}`],
+        this.sm && this.$style[`col-sm-${this.sm}`],
+        this.vs && this.$style[`col-vs-${this.vs}`],
         this.offset && this.$style[`offset-${this.offset}`],
-        this.offsetXs && this.$style[`offset-xs-${this.offsetXs}`],
-        this.offsetVs && this.$style[`offset-vs-${this.offsetVs}`],
-        this.offsetSm && this.$style[`offset-sm-${this.offsetSm}`],
-        this.offsetSm && this.$style[`offset-sm-${this.offsetSm}`],
-        this.offsetMd && this.$style[`offset-md-${this.offsetMd}`],
+        this.offsetXl && this.$style[`offset-xl-${this.offsetXl}`],
         this.offsetLg && this.$style[`offset-lg-${this.offsetLg}`],
+        this.offsetMd && this.$style[`offset-md-${this.offsetMd}`],
+        this.offsetSm && this.$style[`offset-sm-${this.offsetSm}`],
+        this.offsetVs && this.$style[`offset-vs-${this.offsetVs}`],
         this.order && this.$style[`order-${this.order}`],
         this.orderXl && this.$style[`order-xl-${this.orderXl}`],
         this.orderLg && this.$style[`order-lg-${this.orderLg}`],
         this.orderMd && this.$style[`order-md-${this.orderMd}`],
         this.orderSm && this.$style[`order-sm-${this.orderSm}`],
         this.orderVs && this.$style[`order-vs-${this.orderVs}`],
-        this.orderXs && this.$style[`order-xs-${this.orderXs}`],
-        this.justify && this.$style[`justify-content-${this.justify}`],
-        this.align && this.$style[`align-items-${this.align}`],
-        this.self && this.$style[`align-self-${this.self}`],
-        this.$style.flex,
       ];
     },
     styleGutter() {
