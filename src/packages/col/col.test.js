d5b8bc3f8e254841fb4cf4bd695031464ab658d8
mapuaptu <mapuaptu@gmail.com>
2019-08-08 17:55:24 +0400

    Fix tests for col component.

--- src/packages/col/col.test.js
+++ src/packages/col/col.test.js
@@ -3,31 +3,25 @@ import { mount } from '@vue/test-utils';
 import Col from './index';
 
 describe('Col', () => {
-  it('1. Col - is vue instance', () => {
-    const wrapper = mount(Col);
-
-    expect(wrapper.isVueInstance()).to.equal(true);
-  });
-
-  it('2. Col - can render data attribute', () => {
+  it('1. Col - can render data-test attribute', () => {
     const wrapper = mount(Col);
     const attribute = 'data-test-col';
 
     expect(wrapper.attributes(attribute)).to.equal(attribute);
   });
 
-  it('3. Col - can render text in default slot', () => {
-    const message = 'Col message';
+  it('2. Col - can render content in default slot', () => {
+    const content = 'Col content';
     const wrapper = mount(Col, {
       slots: {
-        default: message,
+        default: content,
       },
     });
 
-    expect(wrapper.text()).to.include(message);
+    expect(wrapper.text()).to.include(content);
   });
 
-  it('4. Col - has paddings when parent(row) have gutter prop.', () => {
+  it('3. Col - has paddings when parent(row) have gutter prop.', () => {
     const gutter = 10;
     const Parent = {
       created() {
