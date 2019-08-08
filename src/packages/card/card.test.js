d058b26f58457583bc345efe6fdc920711e9907d
mapuaptu <mapuaptu@gmail.com>
2019-08-08 17:17:02 +0400

    Fix tests for card component.

--- src/packages/card/card.test.js
+++ src/packages/card/card.test.js
@@ -3,38 +3,32 @@ import { mount } from '@vue/test-utils';
 import Card from './index';
 
 describe('Card', () => {
-  it('1. Card - is vue instance', () => {
-    const wrapper = mount(Card);
-
-    expect(wrapper.isVueInstance()).to.equal(true);
-  });
-
-  it('2. Card - can render data attribute', () => {
+  it('1. Card - can render data-test attribute', () => {
     const wrapper = mount(Card);
     const attribute = 'data-test-card';
 
     expect(wrapper.attributes(attribute)).to.equal(attribute);
   });
 
-  it('3. Card - can render text in default slot', () => {
-    const message = 'Card message';
+  it('2. Card - can render content in default slot', () => {
+    const content = 'Card content';
     const wrapper = mount(Card, {
       slots: {
-        default: message,
+        default: content,
       },
     });
 
-    expect(wrapper.text()).to.include(message);
+    expect(wrapper.text()).to.include(content);
   });
 
-  it('4. Card - can render text in title slot', () => {
-    const message = 'Card message';
+  it('3. Card - can render content in title slot', () => {
+    const content = 'Card content';
     const wrapper = mount(Card, {
       slots: {
-        title: message,
+        title: content,
       },
     });
 
-    expect(wrapper.text()).to.include(message);
+    expect(wrapper.text()).to.include(content);
   });
 });
