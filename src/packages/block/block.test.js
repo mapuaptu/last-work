d16d204a209ec3eddce1baf5ffc9663d1b583b6b
mapuaptu <mapuaptu@gmail.com>
2019-08-08 16:55:50 +0400

    Remove test for vue-instance from block component.

--- src/packages/block/block.test.js
+++ src/packages/block/block.test.js
@@ -3,27 +3,21 @@ import { mount } from '@vue/test-utils';
 import Block from './index';
 
 describe('Block', () => {
-  it('1. Block - is vue instance', () => {
-    const wrapper = mount(Block);
-
-    expect(wrapper.isVueInstance()).to.equal(true);
-  });
-
-  it('2. Block - can render data attribute', () => {
+  it('1. Block - can render data-test attribute', () => {
     const wrapper = mount(Block);
     const attribute = 'data-test-block';
 
     expect(wrapper.attributes(attribute)).to.equal(attribute);
   });
 
-  it('3. Block - can render text in default slot', () => {
-    const message = 'Block message';
+  it('2. Block - can render content in default slot', () => {
+    const content = 'Block content';
     const wrapper = mount(Block, {
       slots: {
-        default: message,
+        default: content,
       },
     });
 
-    expect(wrapper.text()).to.include(message);
+    expect(wrapper.text()).to.include(content);
   });
 });
