d77a0035cf121c2be169a7414f77bc5fedb5c43b
mapuaptu <mapuaptu@gmail.com>
2019-08-08 18:09:40 +0400

    Fix tests for container component.

--- src/packages/container/container.test.js
+++ src/packages/container/container.test.js
@@ -3,27 +3,21 @@ import { mount } from '@vue/test-utils';
 import Container from './index';
 
 describe('Container', () => {
-  it('1. Container - is vue instance', () => {
-    const wrapper = mount(Container);
-
-    expect(wrapper.isVueInstance()).to.equal(true);
-  });
-
-  it('2. Container - can render data attribute', () => {
+  it('1. Container - can render data-test attribute', () => {
     const wrapper = mount(Container);
     const attribute = 'data-test-container';
 
     expect(wrapper.attributes(attribute)).to.equal(attribute);
   });
 
-  it('3. Container - can render text in default slot', () => {
-    const message = 'Container message';
+  it('2. Container - can render content in default slot', () => {
+    const content = 'Container content';
     const wrapper = mount(Container, {
       slots: {
-        default: message,
+        default: content,
       },
     });
 
-    expect(wrapper.text()).to.include(message);
+    expect(wrapper.text()).to.include(content);
   });
 });
