054bc2d0f1fcda831d688964f8814fa481c0a293
mapuaptu <mapuaptu@gmail.com>
2019-08-09 17:38:19 +0400

    Fix tests for input component.

--- src/packages/input/input.test.js
+++ src/packages/input/input.test.js
@@ -5,37 +5,25 @@ import Userpic from '../userpic/index';
 import Icon from '../icon/index';
 
 describe('Input', () => {
-  it('1. Input - is vue instance', () => {
-    const wrapper = mount(Input);
-
-    expect(wrapper.isVueInstance()).to.equal(true);
-  });
-
-  it('2. Input - can render data attribute', () => {
+  it('1. Input - can render data-test attribute', () => {
     const wrapper = mount(Input);
     const attribute = 'data-test-input';
 
     expect(wrapper.attributes(attribute)).to.equal(attribute);
   });
 
-  it('3. Input - can render text in default slot', () => {
-    const message = 'Input message';
+  it('2. Input - can render content in default slot', () => {
+    const content = 'Input content';
     const wrapper = mount(Input, {
       slots: {
-        default: message,
+        default: content,
       },
     });
 
-    expect(wrapper.text()).to.include(message);
+    expect(wrapper.text()).to.include(content);
   });
 
-  it('4. Input - can render input tag', () => {
-    const wrapper = mount(Input);
-
-    expect(wrapper.contains('input')).to.equal(true);
-  });
-
-  it('5. Input - can render dd-userpic', () => {
+  it('3. Input - can render dd-userpic', () => {
     const wrapper = mount(Input, {
       propsData: {
         image: 'image',
@@ -45,7 +33,7 @@ describe('Input', () => {
     expect(wrapper.contains(Userpic)).to.equal(true);
   });
 
-  it('6. Input - can render dd-icon', () => {
+  it('4. Input - can render dd-icon', () => {
     const wrapper = mount(Input, {
       provide: {
         root: {
