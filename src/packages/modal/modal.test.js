7eb62b4f6a2cabbc121a58859cb2b8ce1d2bd963
mapuaptu <mapuaptu@gmail.com>
2019-08-09 18:04:22 +0400

    Fix tests for modal component.

--- src/packages/modal/modal.test.js
+++ src/packages/modal/modal.test.js
@@ -3,44 +3,32 @@ import { mount } from '@vue/test-utils';
 import Modal from './index';
 
 describe('Modal', () => {
-  it('1. Modal - is vue instance', () => {
-    const wrapper = mount(Modal);
-
-    expect(wrapper.isVueInstance()).to.equal(true);
-  });
-
-  it('2. Modal - can render data attribute', () => {
+  it('1. Modal - can render data-test attribute', () => {
     const wrapper = mount(Modal);
     const attribute = 'data-test-modal';
 
     expect(wrapper.attributes(attribute)).to.equal(attribute);
   });
 
-  it('3. Modal - can render text in default slot', () => {
-    const message = 'Modal message';
+  it('2. Modal - can render content in default slot', () => {
+    const content = 'Modal content';
     const wrapper = mount(Modal, {
       slots: {
-        default: message,
+        default: content,
       },
     });
 
-    expect(wrapper.text()).to.include(message);
+    expect(wrapper.text()).to.include(content);
   });
 
-  it('4. Modal - can render text in control slot', () => {
-    const message = 'Modal message';
+  it('3. Modal - can render content in control slot', () => {
+    const content = 'Modal content';
     const wrapper = mount(Modal, {
       slots: {
-        control: message,
+        control: content,
       },
     });
 
-    expect(wrapper.text()).to.include(message);
-  });
-
-  it('5. Modal - can render i tag for close icon', () => {
-    const wrapper = mount(Modal);
-
-    expect(wrapper.contains('i')).to.equal(true);
+    expect(wrapper.text()).to.include(content);
   });
 });
