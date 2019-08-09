0361a50e52aa203c9e3036dad6799b6faae7ef79
mapuaptu <mapuaptu@gmail.com>
2019-08-09 17:34:01 +0400

    Fix tests for icon component.

--- src/packages/icon/icon.test.js
+++ src/packages/icon/icon.test.js
@@ -3,33 +3,21 @@ import { mount } from '@vue/test-utils';
 import Icon from './index';
 
 describe('Icon', () => {
-  it('1. Icon - is vue instance', () => {
-    const wrapper = mount(Icon);
-
-    expect(wrapper.isVueInstance()).to.equal(true);
-  });
-
-  it('2. Icon - can render data attribute', () => {
+  it('1. Icon - can render data-test attribute', () => {
     const wrapper = mount(Icon);
     const attribute = 'data-test-icon';
 
     expect(wrapper.attributes(attribute)).to.equal(attribute);
   });
 
-  it('3. Icon - can render text in default slot', () => {
-    const message = 'Icon message';
+  it('2. Icon - can render content in default slot', () => {
+    const content = 'Icon content';
     const wrapper = mount(Icon, {
       slots: {
-        default: message,
+        default: content,
       },
     });
 
-    expect(wrapper.text()).to.include(message);
-  });
-
-  it('4. Icon - can render i tag', () => {
-    const wrapper = mount(Icon);
-
-    expect(wrapper.contains('i')).to.equal(true);
+    expect(wrapper.text()).to.include(content);
   });
 });
