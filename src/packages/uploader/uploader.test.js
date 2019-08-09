acc5bbbfdaae345b500773135aafe678076d1734
mapuaptu <mapuaptu@gmail.com>
2019-08-09 18:43:00 +0400

    Fix tests for uploader component.

--- src/packages/uploader/uploader.test.js
+++ src/packages/uploader/uploader.test.js
@@ -3,48 +3,36 @@ import { mount } from '@vue/test-utils';
 import Uploader from './index';
 
 describe('Uploader', () => {
-  it('1. Uploader - is vue instance', () => {
-    const wrapper = mount(Uploader);
-
-    expect(wrapper.isVueInstance()).to.equal(true);
-  });
-
-  it('2. Uploader - can render data attribute', () => {
+  it('1. Uploader - can render data-test attribute', () => {
     const wrapper = mount(Uploader);
     const attribute = 'data-test-uploader';
 
     expect(wrapper.attributes(attribute)).to.equal(attribute);
   });
 
-  it('3. Uploader - can render text in default slot', () => {
-    const message = 'Uploader message';
+  it('2. Uploader - can render content in default slot', () => {
+    const content = 'Uploader content';
     const wrapper = mount(Uploader, {
       slots: {
-        default: message,
+        default: content,
       },
     });
 
-    expect(wrapper.text()).to.include(message);
+    expect(wrapper.text()).to.include(content);
   });
 
-  it('4. Uploader - can render text in image slot', () => {
-    const message = 'Uploader message';
+  it('3. Uploader - can render content in image slot', () => {
+    const content = 'Uploader content';
     const wrapper = mount(Uploader, {
       slots: {
-        image: message,
+        image: content,
       },
     });
 
-    expect(wrapper.text()).to.include(message);
+    expect(wrapper.text()).to.include(content);
   });
 
-  it('5. Uploader - can render input', () => {
-    const wrapper = mount(Uploader);
-
-    expect(wrapper.contains('input')).to.equal(true);
-  });
-
-  it('6. Uploader - can delete image', () => {
+  it('4. Uploader - delete button can trigger emit("delete")', () => {
     const wrapper = mount(Uploader, {
       slots: {
         image: '<img></img>',
