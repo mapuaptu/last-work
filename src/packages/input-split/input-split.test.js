a779d1ce336ff1ae3d40727d2fc6cd6a25bf1bc4
mapuaptu <mapuaptu@gmail.com>
2019-08-09 17:43:05 +0400

    Fix test for input-split component.

--- src/packages/input-split/input-split.test.js
+++ src/packages/input-split/input-split.test.js
@@ -3,27 +3,21 @@ import { mount } from '@vue/test-utils';
 import InputSplit from './index';
 
 describe('InputSplit', () => {
-  it('1. InputSplit - is vue instance', () => {
-    const wrapper = mount(InputSplit);
-
-    expect(wrapper.isVueInstance()).to.equal(true);
-  });
-
-  it('2. InputSplit - can render data attribute', () => {
+  it('1. InputSplit - can render data-test attribute', () => {
     const wrapper = mount(InputSplit);
     const attribute = 'data-test-input-split';
 
     expect(wrapper.attributes(attribute)).to.equal(attribute);
   });
 
-  it('3. InputSplit - can render text in default slot', () => {
-    const message = 'InputSplit message';
+  it('2. InputSplit - can render content in default slot', () => {
+    const content = 'InputSplit content';
     const wrapper = mount(InputSplit, {
       slots: {
-        default: message,
+        default: content,
       },
     });
 
-    expect(wrapper.text()).to.include(message);
+    expect(wrapper.text()).to.include(content);
   });
 });
